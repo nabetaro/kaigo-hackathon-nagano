ed5139bfea925a1a4701286073ff6a6a018452cc
KURASAWA Nozomu <nabetaro@caldron.jp>
2020-02-23 22:40:02 +0900

    add default user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+unless User.find(1)
+  User.create(id: 1, email: 'admin@localhost')
+end
